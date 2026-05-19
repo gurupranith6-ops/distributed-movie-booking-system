73680b9c561135d7de618ddfe2abaf3d8bf0c673
Guru Praneeth <gurupranith6@gmail.com>
2026-05-19 22:22:33 +0530

    fixed tls

--- backend/config/redis.js
+++ backend/config/redis.js
@@ -1,16 +1,27 @@
 const redis = require('redis');
 
 const redisClient = redis.createClient({
-    url: process.env.REDIS_URL
+    url: process.env.REDIS_URL,
+    socket: {
+        tls: true,
+        rejectUnauthorized: false
+    }
 });
 
 const connectRedis = async () => {
 
     try {
 
+        console.log(
+            'REDIS URL:',
+            process.env.REDIS_URL
+        );
+
         await redisClient.connect();
 
-        console.log('Redis Connected');
+        console.log(
+            'Redis Connected'
+        );
 
     } catch (error) {
 
